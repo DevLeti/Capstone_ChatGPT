b5ba5dd60841b9f3bf15337d5bee97e67e3aa743
DevLeti <devleti0106@gmail.com>
2023-12-13 17:30:18 +0900

    EDIT: Calculate Average, Max, Min

--- answerScore.py
+++ answerScore.py
@@ -21,7 +21,6 @@ for row in range(1,11):
     score1 = sentence_bleu(reference, candidate, weights=(1, 0, 0, 0)) # 1.0
     score1_bleu_arr.append(score1)
 
-# print(score1_bleu_arr)
 
 # 2. "전청조의 사기 액수를 알려줘."
 reference = ["천청조의 사기 액수는 36억 9000여만원입니다.".split(),"전청조는 사기를 통해 약 37억원의 피해액을 발생시켰습니다.".split(), "전청조에 의해 발생한 피해액은 36억9000여만원 입니다.".split()]
@@ -31,7 +30,6 @@ for row in range(11,21):
     score2 = sentence_bleu(reference, candidate, weights=(1, 0, 0, 0))  # 1.0
     score2_bleu_arr.append(score2)
 
-# print(score2_bleu_arr)
 
 """
 1-2. 막대그래프로 표현
@@ -56,6 +54,22 @@ plt.yticks(np.arange(0,1.1,0.1))
 
 plt.suptitle("Bleu Score")
 plt.show()
+
+# print(score1_bleu_arr)
+score1_bleu_avg = 0
+for i in score1_bleu_arr:
+    score1_bleu_avg += i
+score1_bleu_avg = score1_bleu_avg / len(score1_bleu_arr)
+
+# print(score1_bleu_arr)
+# print(avg)
+
+# print(score2_bleu_arr)
+score2_bleu_avg = 0
+for i in score2_bleu_arr:
+    score2_bleu_avg += i
+score2_bleu_avg = score2_bleu_avg / len(score2_bleu_arr)
+# print(avg)
 
 """
 2-1. TF-IDF + Cosine Similarity
@@ -110,5 +124,28 @@ plt.title("전청조의 사기 액수")
 plt.bar(x, score2_cs_arr)
 plt.yticks(np.arange(0,1.1,0.1))
 
+score1_cs_avg = 0
+for i in score1_cs_arr:
+    score1_cs_avg += i
+score1_cs_avg = score1_cs_avg / len(score1_cs_arr)
+# print(score1_cs_arr)
+# print(avg)
+
+score2_cs_avg = 0
+for i in score2_cs_arr:
+    score2_cs_avg += i
+score2_cs_avg = score2_cs_avg / len(score2_cs_arr)
+# print(avg)
+
 plt.suptitle("TF-IDF + Cosine Similarity")
 plt.show()
+
+print(f"Score1(BLEU Score)")
+print(f"Min: {round(min(score1_bleu_arr),2)}, Max: {round(max(score1_bleu_arr),2)}, Avg: {round(score1_bleu_avg,2)}")
+print(f"Score2(BLEU Score)")
+print(f"Min: {round(min(score2_bleu_arr),2)}, Max: {round(max(score2_bleu_arr),2)}, Avg: {round(score2_bleu_avg,2)}")
+
+print(f"Score1(CS)")
+print(f"Min: {round(min(score1_cs_arr),2)}, Max: {round(max(score1_cs_arr),2)}, Avg: {round(score1_cs_avg,2)}")
+print(f"Score2(CS)")
+print(f"Min: {round(min(score2_cs_arr),2)}, Max: {round(max(score2_cs_arr),2)}, Avg: {round(score2_cs_avg,2)}")
